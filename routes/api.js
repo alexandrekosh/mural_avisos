12b2bd14d16c8ca99e254b82d4d40af072cd31e7
Alexandre Medeiros <alexandre.koshy@gmail.com>
2023-05-13 21:56:09 -0300

    add cors

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,22 @@
 const express = require('express') //expresse
 const router = express.Router() // router
 const bodyParser = require('body-parser') //body-parse
+const cors = require('cors') // cors
 const post = require('../model/post') // post
+
+// const options = {
+//     origin: 'http://localhost:3000',
+// }
+
+// router.use(cors(options))
 
 // buscar por todos os posts
 router.get("/all", (req, res) => {
-
     res.json(JSON.stringify(post.getAll()))
-
 })
 
 // criar novo post
 router.post("/new", bodyParser.json(), (req, res) => {
-
     let title = req.body.title
     let description = req.body.description
     post.newPost(title, description)
